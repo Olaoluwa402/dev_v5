4bbb6270865732ae47763e8759cdb8c31c5f969e
olaoluwa402 <ibukundaniel402@yahoo.co.uk>
2023-12-14 14:35:12 +0100

    expense tracker frontend app - react

--- nodejs/expense_tracker/src/config/db.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export const dbConnect = () => {
-  const conn = mongoose.connect(process.env.MONGO_URI);
-  return conn; 
-};
--- nodejs/expense_tracker/src/controllers/IncomeExpense/incomeExpenseSchema.js
+++
@@ -1,8 +0,0 @@
-import Joi from "joi";
-
-export const incomeExpenseSchema = Joi.object({
-  amount: Joi.number().required(),
-  type: Joi.string().valid("income", "expense").required(),
-  desc: Joi.string().required(),
-  categoryId: Joi.string().required(),
-});
