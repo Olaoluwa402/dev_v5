8ff568da7dece6cd05d8c9463c24f15a616e4d9f
olaoluwa402 <ibukundaniel402@yahoo.co.uk>
2023-12-15 12:28:17 +0100

    register endpoint

--- nodejs/expense_tracker_frontend/src/App.js
+++ nodejs/expense_tracker_frontend/src/App.js
@@ -1,7 +1,23 @@
+import HomePage from "./pages/home/home";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
   return (
     <div className="App">
-      <h1>Expense Tracker APP</h1>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <HomePage />
     </div>
   );
 }
